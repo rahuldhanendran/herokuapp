ea9c38270c597845521f78c1ea08587d0b58ab44
Rahul Dhanendran <rahul.dhanendran@evariant.com>
2016-12-12 11:51:22 -0600

    23

--- public/ixn/activities/hello-world/config.js
+++ public/ixn/activities/hello-world/config.js
@@ -6,7 +6,7 @@ define([], function(){
         "partnerApiObjectTypeId": "IXN.CustomActivity.REST",
         "lang": {
             "en-US": {
-                "name": "Hello World Activity 2",
+                "name": "Direct Mail",
                 "description": "Activity simply posts the data into an array for display on the App's home page."
             }
         },
